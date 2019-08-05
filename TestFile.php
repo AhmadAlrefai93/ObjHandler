8a95b7da1efe9a639bb6680c8797f6be179e4d3f
AhmadAlrefai93 <53712179+AhmadAlrefai93@users.noreply.github.com>
2019-08-06 00:14:44 +0300

    Update TestFile.php

--- TestFile.php
+++ TestFile.php
@@ -3,42 +3,45 @@
 require 'ObjectHandler.php';
 
 
-class User {
- 
+class User
+{
+
 	private $name;
 	private $age;
- 
-	function __construct( $name, $age ) {
+
+	function __construct($name, $age)
+	{
 		$this->name = $name;
 		$this->age = $age;
 	}
- 
-	function getName() {
+
+	function getName()
+	{
 		return $this->name;
 	}
- 
-	function isAdult() {
-		return $this->age >= 18?"an Adult":"Not an Adult";
+
+	function isAdult()
+	{
+		return $this->age >= 18 ? "an Adult" : "Not an Adult";
 	}
 
 	public function __toString()
-    {
-        return (string) $this->name. (string)$this->age;
-    }
- 
+	{
+		return (string) $this->name . (string) $this->age;
+	}
 }
 
-$h = new User( "Ali", 15 );
+$h = new User("Ali", 15);
 
-$i = new User( "Khaled", 22 );
+$i = new User("Khaled", 22);
 
-$j = new User( "Mohammad", 27 );
+$j = new User("Mohammad", 27);
 
-$k = new User( "Anas", 19 );
+$k = new User("Anas", 19);
 
-$l = new User( "Ahmad", 26 );
+$l = new User("Ahmad", 26);
 
-$m = new User( "saif", 18 );
+$m = new User("saif", 18);
 
 //$o = new ObjectHandler();
 ObjectHandler::save($h);
@@ -50,4 +53,8 @@ ObjectHandler::save("ahmad");
 ObjectHandler::save(15);
 ObjectHandler::save($m);
 
-echo ObjectHandler::find(5);
+
+echo ObjectHandler::find(2) . '<br>';
+echo ObjectHandler::find(7) . '<br>';
+echo ObjectHandler::find(12) . '<br>';
+echo ObjectHandler::find(1) . '<br>';
